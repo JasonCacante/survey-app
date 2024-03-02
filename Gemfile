6538c04257b2a05e369c30151c49def13f46203b
JasonCacante <jason.cacante@gmail.com>
2024-03-01 22:16:53 -0500

    feat(sendgrid_config): add configuration for sendgrid and others

--- Gemfile
+++ Gemfile
@@ -54,6 +54,9 @@ gem "tailwindcss-rails", "~> 2.0"
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
+
+  # dotenv
+  gem "dotenv-rails"
 end
 
 group :development do
@@ -73,3 +76,6 @@ group :test do
   gem "selenium-webdriver"
 
 end
+
+# devise
+gem "devise"
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/index'
-  root 'pages#landing_page'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
+  authenticated :user do
+    root "pages#index", as: :authenticated_root
+  end
+  unauthenticated :user do
+    root "pages#landing_page"
+  end
 end
